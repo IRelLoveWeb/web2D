5bef592f8cb85fdaca8e63fc15aec5a00b40b5da
lyf_lyf <liuyingfeng@blued.com>
2019-05-31 10:28:29 +0800

    feat: 学习shell脚本

---
+++ shell学习/op.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 数学运算 表达式和运算符之间需要有空格
+val=`expr 2 + 2`
+echo "数学运算加法'expr 2 + 2': ${val}"
+
+val=`expr 3 \* 3`
+val2=`expr 3 / 3`
+echo "数学运算乘/除法'expr 3 (\* | /) 3': ${val},${val2}"
--- shell学习/variable.sh
+++ shell学习/variable.sh
@@ -5,6 +5,13 @@
 varyone=123
 echo $varyone
 
+# 只读变量
+v2=1
+readonly v2 # 将变量v2设置为只读的
+
+# 删除变量
+unset v2
+
 # 重新赋值变量
 varone=01234567891011
 echo $varone
@@ -23,10 +30,18 @@ echo "获取字符串长度,${#varone}"
 
 echo "截取部分(序号3-5)字符,${varone:3:5}"
 
+str="abcdefg"
+echo `expr index "$str" fc`
 
 # 定义数组
 varyarr=(1 i2 dd3 fff4 ggggA ffffB C D)
 
 echo "输出数组下标为3的值,\${varyarr[3]}:${varyarr[3]}"
-echo "获取数组的长度,\${#varyarr[*]}:${#varyarr[*]}"
+echo "获取数组的长度,\${#varyarr[*/@]}:${#varyarr[*]}"
 echo "获取数组下标为2的字符长度,\${#varyarr[2]}:${#varyarr[2]}"
+
+# 循环输出数组
+for str in ${varyarr[@]};
+do
+  echo $str
+done
