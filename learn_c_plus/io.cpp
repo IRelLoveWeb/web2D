53c6b2b89a306053612dd898868145fc8c457213
lyf_lyf <liuyingfeng@blued.com>
2019-05-31 10:49:50 +0800

    feat: 学习c++

---
+++ learn_c_plus/io.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main() {
+  // std::cout << "输入两个数字" << std::endl;
+  // int v1 = 0, v2 = 0;
+  // std::cin >> v1 >> v2;
+  // std::cout << "两数子和:" << v1 << "加" << v2 << "等于" << v1 + v2;
+  // std::cout << std::endl;
+
+  int i = -9;
+  unsigned ui = 10;
+  unsigned ui2 = 5;
+
+  std::cout << i - ui2 <<std::endl;
+
+  return -1;
+}
